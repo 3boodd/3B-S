1847b1c2fc5b7ea5e80459e677349ebe54201bc7
3boodd <36028899+3boodd@users.noreply.github.com>
2018-07-19 22:55:28 +0400

    Update copy.js

--- copy.js
+++ copy.js
@@ -1,126 +1,92 @@
-const fs = require('fs');
-const path = require('path');
-const Discord = require('discord.js');
-const VersionControl = require('./objects/versioncontrol');
-const Validator = require('./objects/validator');
-const Serializer = require('./objects/serializer');
-const Cleaner = require('./objects/cleaner');
-const Creator = require('./objects/creator');
-const Logger = require('./objects/logger');
-const settings = require('./settings.json');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
-let isBackup = false;
-let isRestore = false;
-let isClone = false;
-let backupFile = 'guildData.json';
-
-client.on('ready', async () => {
-    if (!client.user.bot) {
-        Logger.logError('Specified user token is not a bot user token.');
-        return process.exit(1);
-    }
-    Logger.logMessage(`Successfully logged in as ${client.user.tag}. Starting script.`);
-
-    let originalGuildId = settings.originalGuildId;
-    let newGuildId = settings.newGuildId;
-    let newGuildAdminRoleId = settings.newGuildAdminRoleId;
-    let guildData = { step: 1 };
-
-    try {
-        // Check discord.js version
-        let djsVersion = require('./node_modules/discord.js/package.json').version;
-        if (djsVersion !== '12.0.0-dev') throw new Error('Please don\'t install discord.js with \'npm install discord.js\'! Installation instructions are in the README.');
-
-        // Check script version
-        let { version } = require('./package.json');
-        let result = await VersionControl.checkVersion(version).catch(err => {
-            return { error: err || new Error('failed') };
-        });
-        if (result.error) console.log(`${result.error}\nScript execution will resume.`)
-        else if (version !== result.version) throw new Error(`You are not using the latest script version. Please redownload the repository.`
-            + `\nYour version: ${version}\nLatest version: ${result.version}`);
-        if (!result.error) console.log('Latest script version installed');
-
-        // Settings Validation only on restore or clone
-        let data = { changed: false };
-        if (!isBackup) data = Validator.validateSettings(client, originalGuildId, newGuildId, newGuildAdminRoleId);
-        if (data.changed) newGuildAdminRoleId = data.newGuildAdminRoleId;
-
-        // Load/Create serialized guildData
-        if (fs.existsSync(backupFile) && isRestore) {
-            guildData = require(`./${backupFile}`);
-            guildData.step = 1;
-            Logger.logMessage(`${guildData.step++}. Serialized data was found and will be used.`);
-        } else if (isRestore) {
-            throw new Error(`Specified restore but guild backup '${backupFile}' doesn't exist.`);
-        } else {
-            if (!client.guilds.has(originalGuildId)) {
-                throw new Error('Original guild to copy does not exist. Please check if the id in the ' +
-                    'settings is correct and if the bot is also member of this guild.');
-            }
-            let banCollection = new Discord.Collection();
-            try {
-                if (settings.copyBans) banCollection = await client.guilds.get(originalGuildId).fetchBans();
-            } catch (banError) {
-                throw new Error('You tried to copy bans without giving the bot the BAN_MEMBERS permissions on the original guild.');
-            }
-            guildData = Serializer.serializeOldGuild(client, originalGuildId, banCollection, guildData, backupFile);
-        }
-
-        // Stop on backup only
-        if (isBackup) {
-            Logger.logMessage(`${guildData.step}. Program execution finished because backup was specified.`);
-            await client.destroy();
-            return process.exit();
-        }
-
-        // Cleanup new guild
-        guildData = await Cleaner.cleanNewGuild(client, newGuildId, newGuildAdminRoleId, guildData);
-
-        // Create new guild
-        await Creator.setData(client, guildData, newGuildId, newGuildAdminRoleId);
-    } catch (err) {
-        Logger.logError(err);
-    }
-
-    await client.destroy();
-    return process.exit();
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('rateLimit', rateLimitObj => {
-    if (settings.debug) {
-        Logger.logMessage(`Rate limit reached!\nTimeout: ${rateLimitObj.timeout}\nLimit: ${rateLimitObj.limit}\n` +
-            `TimeDiff: ${rateLimitObj.timeDifference}\nMethod: ${rateLimitObj.method}\nPath: ${rateLimitObj.path}\n` +
-            `Route: ${rateLimitObj.route}`);
-    }
+
+
+client.on('message', function(message) {
+    if(message.content.startsWith(prefix + "ابلاغ")) {
+        let messageArgs = message.content.split(" ").slice(1).join(" ");
+        let messageReason = message.content.split(" ").slice(2).join(" ");
+        if(!messageReason) return message.reply("**# Specify a reason!**");
+    let mUser = message.mentions.users.first();
+    if(!mUser) return message.channel.send("Couldn't find user.");
+    let Rembed = new Discord.RichEmbed()
+    .setTitle("`New Report!`")
+    .setThumbnail(message.author.avatarURL)
+    .addField("**# - Reported User:**",mUser,true)
+    .addField("**# - Reported User ID:**",mUser.id,true)
+    .addField("**# - Reason:**",messageReason,true)
+    .addField("**# - Channel:**",message.channel,true)
+    .addField("**# - Time:**",message.createdAt,true)
+    .setFooter("لو ان الابلاغ فيه مزح راح يتعرض صاحب الابلاغ لقوبات")
+message.channel.send(Rembed)
+message.channel.send("__Are you sure you want to send this to the Server owner??__").then(msg => {
+    msg.react("✅")
+    msg.react("❌")
+.then(() => msg.react('❌'))
+.then(() =>msg.react('✅'))
+let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+
+let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+reaction1.on("collect", r => {
+    message.guild.owner.send(Rembed)
+    message.reply("**# - Done! 🎇**");
+})
+reaction2.on("collect", r => {
+    message.reply("**# - Canceled!**");
+})
+})
+}
 });
 
-function printUsage() {
-    console.log(
-        `Usage:
-  * Backup guild to file: node copy.js backup <backupFile (optional)>
-  * Restore guild from file: node copy.js restore <backupFile (optional)>
-  * Clone guild to guild: node copy.js clone`
-    );
-    process.exit(1);
-}
 
-function main() {
-    const args = process.argv.slice(2)
-    if (args.length < 1 || !['backup', 'restore', 'clone'].includes(args[0])) {
-        printUsage();
-    } else if (args.length >= 2 && ['backup', 'restore'].includes(args[0])) {
-        if (path.extname(args[1]) === '.json') {
-            backupFile = args[1];
-        } else {
-            backupFile = args[1] + '.json';
-        }
-    }
-    isBackup = args[0] === 'backup';
-    isRestore = args[0] === 'restore';
-    isClone = args[0] === 'clone';
-    client.login(settings.NDY3Njk0NTYyMTAyNDc2ODEw.Di0IbQ.n0bnpZgjX6iKwBxBjfcJCJ_EsB8);
-}
 
-main();
+
+  let channel = ["469575636432846870"];
+client.on('voiceStateUpdate', (Codes, ReBeL) => {
+client.channels.get(channel);
+    ReBeL.guild.member(ReBeL).addRole(ReBeL.guild.roles.find("name", "ولد"))
+                  ReBeL.guild.createChannel('ReBeL', 'voice').then(c => {
+                ReBeL.setVoiceChannel(c).then(() => {
+                    c.delete(305).catch(console.log);
+  console.log("Done");
+});
+});
+});
+  
+
+
+  let channel = ["469575710558650388"];
+client.on('voiceStateUpdate', (Codes, ReBeL) => {
+client.channels.get(channel);
+    ReBeL.guild.member(ReBeL).addRole(ReBeL.guild.roles.find("name", "بنت"))
+                  ReBeL.guild.createChannel('ReBeL', 'voice').then(c => {
+                ReBeL.setVoiceChannel(c).then(() => {
+                    c.delete(305).catch(console.log);
+  console.log("Done");
+});
+});
+});
+  
+  client.on('message', async message => {
+  if(message.content.startsWith(prefix + "اقتراح")) {
+  await  message.channel.send(`اكتب اقتراحك الان`)
+    let filter = m => m.author.id === message.author.id
+      var text = '';
+        let sugsa = message.channel.awaitMessages(filter, { max: 1, time: 60000})
+          .then(co => {
+            text = co.first().content
+
+              message.channel.send(`تم حفظ اقتراحك الرجاء انتضار الرد من قبل الاداره`)
+                client.channels.get("469577187281076234").send(`${message.author.username}'s sug => ${text}`)
+
+              })
+            }
+          })
+  
