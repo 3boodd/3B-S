bb9e7f1fa91adaf2b502f9709ebe8227725cf76b
3boodd <36028899+3boodd@users.noreply.github.com>
2018-07-20 04:47:26 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,5 +24,19 @@ var prefix = "3";
             }
           })
   
+
+  let channel = ["469575636432846870"];
+client.on('voiceStateUpdate', (Codes, ReBeL) => {
+client.channels.get(channel);
+    ReBeL.guild.member(ReBeL).addRole(ReBeL.guild.roles.find("name", "ولد"))
+                  ReBeL.guild.createChannel('ReBeL', 'voice').then(c => {
+                ReBeL.setVoiceChannel(c).then(() => {
+                    c.delete(305).catch(console.log);
+  console.log("Done");
+});
+});
+});
+
+
   
    client.login('NDY5NjU5NzU4NDAwMzcyNzM2.DjK9IQ.4AvTn_4OIIIQaKeD6RCF_5FMbsM');
