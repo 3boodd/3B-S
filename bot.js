71ee5e3e4d4ab2a30f5892315ae16d5619e69546
3boodd <36028899+3boodd@users.noreply.github.com>
2018-07-20 05:14:28 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,28 +8,12 @@ client.on('ready', () => {
 
 var prefix = "3";
 
-  client.on('message', async message => {
-  if(message.content.startsWith(prefix + "اقتراح")) {
-  await  message.channel.send(`اكتب اقتراحك الان`)
-    let filter = m => m.author.id === message.author.id
-      var text = '';
-        let sugsa = message.channel.awaitMessages(filter, { max: 1, time: 60000})
-          .then(co => {
-            text = co.first().content
-
-              message.channel.send(`تم حفظ اقتراحك الرجاء انتضار الرد من قبل الاداره`)
-                client.channels.get("469577187281076234").send(`${message.author.username}'s sug => ${text}`)
-
-              })
-            }
-          })
-  
 
   let channel = ["469575636432846870"];
 client.on('voiceStateUpdate', (Codes, ReBeL) => {
 client.channels.get(channel);
     ReBeL.guild.member(ReBeL).addRole(ReBeL.guild.roles.find("name", "ولد"))
-                  ReBeL.guild.createChannel('ReBeL', 'voice').then(c => {
+                  ReBeL.guild.createChannel('3B', 'voice').then(c => {
                 ReBeL.setVoiceChannel(c).then(() => {
                     c.delete(305).catch(console.log);
   console.log("Done");
