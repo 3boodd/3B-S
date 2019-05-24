f54f574450d106dc9bf938e616503abd74b986e4
3boodd <36028899+3boodd@users.noreply.github.com>
2019-05-24 23:10:18 +0200

    Delete bot.js

--- bot.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-  
-   client.login('NDY5NjU5NzU4NDAwMzcyNzM2.DjK9IQ.4AvTn_4OIIIQaKeD6RCF_5FMbsM');
